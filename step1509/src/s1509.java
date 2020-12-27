18e5f0c0f53e8e4c48ab9eff9bf92a73b1b29ea1
Aleksey <aleks-sh@mail.ru>
2020-12-27 03:58:20 +0300

    1509

--- step1509/src/s1509.java
+++ step1509/src/s1509.java
@@ -6,27 +6,22 @@ public class s1509 {
         Scanner in = new Scanner("1 51 61 51 51 3153 48111 2 121 51 8 1 51 5 1 1 5\n" +
                 "51 515 1181 1 1 512 215 15 151 8 98 54 545 48 8 78 945191 91 ");
         ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arrInt1 = new ArrayList<>();
+        ArrayList<Integer> arrInt2 = new ArrayList<>();
         String[] arr1 = in.nextLine().split(" ");
+        for (String str : arr1)
+            arrInt1.add(Integer.parseInt(str));
         String[] arr2 = in.nextLine().split(" ");
-        for (String s : arr1) {
-            if (contStr(arr2, s))
-                if (!contInt(arr, Integer.parseInt(s)))
-                    arr.add(Integer.parseInt(s));
-        }
-        arr.sort((a, b) -> a - b);
-        for (int i : arr)
-            System.out.print(i + " ");
-    }
+        for (String str : arr2)
+            arrInt2.add(Integer.parseInt(str));
 
-    static boolean contStr(String[] arr, String str) {
-        for (String s : arr)
-            if (s.equals(str)) return true;
-        return false;
-    }
-
-    static boolean contInt(ArrayList<Integer> arr, int i) {
-        for (int s : arr)
-            if (s == i) return true;
-        return false;
+        arrInt1.retainAll(arrInt2);
+        arrInt1.sort((a,b)->a-b);
+        arrInt2.clear();
+        for (int i:arrInt1)
+            if(!arrInt2.contains(i))
+                arrInt2.add(i);
+        for (int i:arrInt2)
+            System.out.print(i+" ");
     }
 }
