81583134a477af3605b8ffde7f82480cf1703b91
aleks-sh <aleks-sh@mail.ru>
2021-01-26 08:55:06 +0300

    1511

---
+++ step1511/src/s1511.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class s1511 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner("abcde");
+        String str = in.nextLine();
+    }
+}
